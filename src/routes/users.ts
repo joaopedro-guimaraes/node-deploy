e6399255335928a7dc75071ae9ef442658a99245
João Pedro Guimarães <joaopedro.enoki@gmail.com>
2023-07-08 08:29:28 -0300

    Add User email

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,28 +1,43 @@
 import { PrismaClient } from '@prisma/client'
 import { Router } from 'express'
 import { z } from 'zod'
+import bcrypt from 'bcrypt'
 
 const router = Router()
 
 const prisma = new PrismaClient()
 
 router.get('/', async (request, response) => {
-    const users = await prisma.user.findMany()
-    return response.send(users)
+  console.log('GET /users')
+  const users = (await prisma.user.findMany()).map(user => ({
+    id: user.id,
+    name: user.name,
+    email: user.email
+  }))
+  return response.json(users)
 })
 
 router.post('/', async (request, response) => {
-    const createUserSchema = z.object({
-        name: z.string(),
-        email: z.string().email()
-    })
-    const { name, email } = createUserSchema.parse(request.body)
+  console.log('POST /users')
+  const createUserSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string()
+  })
+  const { name, email, password } = createUserSchema.parse(request.body)
 
-    await prisma.user.create({
-        data: { name, email }
-    })
+  const salt = await bcrypt.genSalt(12)
+  const hash = await bcrypt.hash(password, salt)
 
-    return response.status(201).send()
+  await prisma.user.create({
+    data: { 
+      name, 
+      email, 
+      password: hash 
+    }
+  })
+
+  return response.status(201).send()
 })
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,10 @@ app.use(express.json())
 app.use('/users', users)
 
 app.get('/test', (request, response) => {
-    return response.status(200).send({ message: 'OK' })
+  console.log('GET /test')
+  return response.status(200).send({ message: 'OK' })
 })
 
 app.listen(process.env.PORT ? Number(process.env.PORT) : 3333, () => {
-    console.log('HTTP Server Running')
+  console.log('HTTP Server Running')
 })
